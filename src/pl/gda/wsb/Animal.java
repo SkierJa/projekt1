593ad0738fe5278fdd7c3da4eb7b0aaccb2bac90
SkierJa <robinsaon@gmail.com>
2021-02-28 13:46:09 +0100

    Hello World

--- src/pl/gda/wsb/Animal.java
+++ src/pl/gda/wsb/Animal.java
@@ -3,9 +3,11 @@ package pl.gda.wsb;
 public class Animal {
     String name;
     String region;
+    int weight;
 
-    public Animal(String name, String region) {
+    public Animal(String name, String region, int weight) {
         this.name = name;
         this.region = region;
+        this.weight = weight;
     }
 }
--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -7,10 +7,15 @@ public class Main {
 
 	Human human1 = new Human("Jan", "Nowak","Janitor",46);
 	Human human2 = new Human("Maciej", "Sierpinski","QA",32);
-    Animal animal1 = new Animal("Tiger","Asia");
-    Animal animal2 = new Animal("Elephant","Africa");
+    Animal animal1 = new Animal("Tiger","Asia",130);
+    Animal animal2 = new Animal("Elephant","Africa",1800);
     Phone phone1 = new Phone("Galaxy S10+","Samsung");
     Phone phone2 = new Phone("iPhone 6s","Apple");
 
+    if (animal1.weight > animal2.weight) {
+            System.out.println(animal1.name + " is heavier");
+        } else {
+            System.out.println(animal2.name + " is heavier");
+        }
     }
 }
